e4fff768ca2dbfa868563bea0c87a4fc0cdf4c6d
sgcodes7471 <official06srinjoy@gmail.com>
2025-03-02 13:24:05 +0530

    integrated stripe

---
+++ app/ui/app/api/stripe/checkout/route.ts
@@ -0,0 +1,38 @@
+import Stripe from 'stripe';
+import { auth } from '@clerk/nextjs/server'
+import { NextRequest, NextResponse } from 'next/server';
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+export async function POST(request: NextRequest) {
+  try {
+
+    // you can implement some basic check here like, is user valid or not
+    const data = await request.json();
+    const priceId = data.priceId;
+    const { userId , redirectToSignIn } = await auth();
+    if (!userId) {
+        //redirect to sign-in page
+        return redirectToSignIn();
+      }
+    const checkoutSession: Stripe.Checkout.Session =
+      await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [
+          {
+            price: priceId,
+            quantity: 1
+          }
+        ],
+        mode: 'payment',
+        success_url: `${process.env.NEXT_BASE_URL}/billing`,
+        cancel_url: `${process.env.NEXT_BASE_URL}/billing`,
+        metadata: {
+          userId,
+          priceId
+        }
+      });
+    return NextResponse.json({ result: checkoutSession, ok: true });
+  } catch (error) {
+    console.log(error);
+    return new NextResponse('Internal Server', { status: 500 });
+  }
+}
